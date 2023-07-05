5d16efe07b322a6400474246fb19ad5bd5590f05
Simone Baptiste <baptistesimone19@gmail.com>
2023-07-05 14:40:56 +0200

    merge control

---
+++ client/out/extension.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const path = require("path");
+const vscode_1 = require("vscode");
+const node_1 = require("vscode-languageclient/node");
+let client;
+function activate(context) {
+    const serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
+    const serverOptions = {
+        run: { module: serverModule, transport: node_1.TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: node_1.TransportKind.ipc,
+        }
+    };
+    const clientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'uniface' }],
+        synchronize: {
+            fileEvents: vscode_1.workspace.createFileSystemWatcher('**/.uni')
+        }
+    };
+    client = new node_1.LanguageClient('languageServerUniface', 'Language Server Uniface', serverOptions, clientOptions);
+    client.start();
+}
+exports.activate = activate;
+function deactivate() {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
